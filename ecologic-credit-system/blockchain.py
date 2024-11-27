e05f8757c1f1632291de2c05c668b11260d4fd03
HugoFioletti <hugo.fioletti@student-cs.fr>
2024-11-27 16:40:06 +0100

    Add files via upload

---
+++ ecologic-credit-system/blockchain.py
@@ -0,0 +1,228 @@
+"""
+This module contains the class Blockchain. A blockchain is a list of blocks and a mempool.
+"""
+import json
+import random
+import config
+import utils
+from block import Block, InvalidBlock
+from transaction import Transaction
+
+
+class Blockchain(object):
+    def __init__(self):
+        self.chain = [Block()]
+        self.mempool = []
+
+    @property
+    def last_block(self):
+        return self.chain[-1]
+
+    def add_transaction(self, transaction):
+        """
+        Add a new transaction to the mempool. Return True if the transaction is valid and not already in the mempool.
+        :param transaction:
+        :return: True or False
+        """
+        if transaction in self.mempool:
+            return False
+        
+        if transaction.message==None or transaction.date==None or transaction.author==None or transaction.vk==None or transaction.signature==None:
+            return False
+        
+        if not transaction.verify():
+            return False
+        
+        if utils.str_to_time(transaction.date) > utils.str_to_time(utils.get_time()):
+            return False
+        
+        self.mempool.append(transaction)
+        return True
+    
+    def new_block(self, block=None):
+        """
+        Create a new block from transactions choosen in the mempool.
+        :param block: The previous block. If None, the last block of the chain is used.
+        :return: The new block
+        """
+        if not block:
+            block = self.last_block
+
+        transactions = random.sample(self.mempool, min(len(self.mempool), config.blocksize))
+        new_block = block.next(transactions)
+
+        return new_block
+
+    def extend_chain(self, block):
+        """
+        Add a new block to the chain if it is valid (index, previous_hash, proof).
+        :param block: A block
+        :raise InvalidBlock if the block is invalid
+        """
+        if (block.index == self.last_block.index + 1 
+            and block.previous_hash == self.last_block.hash() 
+            and block.valid_proof()):
+
+            self.chain.append(block)
+
+        else:
+            print(block.index > self.last_block.index)
+            print(block.previous_hash == self.last_block.hash())
+            print(block.proof)
+            raise InvalidBlock
+
+    def __str__(self):
+        """
+        String representation of the blockchain
+        :return: str
+        """
+        string = '-----------Blockchain-----------'
+        for block in self.chain:
+            string+= '\n' + str(block)
+        # string+= '\n-----------Mempool-----------'
+        # for trans in self.mempool:
+        #     string+= '\n' + str(trans)
+        return string
+
+    def validity(self):
+        """
+        Check the validity of the chain.
+        - The first block must be the genesis block
+        - Each block must be valid
+        - Each block must point to the previous one
+        - A transaction can only be in one block
+        :return: True if the chain is valid, False otherwise
+        """
+        if self.chain[0].index != 0:
+            return False
+
+        for i in range(1, len(self.chain)):
+            current_block = self.chain[i]
+            previous_block = self.chain[i - 1]
+
+            if not current_block.validity():
+                return False
+
+            if current_block.previous_hash != previous_block.hash():
+                return False
+
+            transactions_hashes = [transaction.hash() for transaction in current_block.transactions]
+            for j in range(i - 1, -1, -1):
+                previous_block = self.chain[j]
+                previous_transactions_hashes = [transaction.hash() for transaction in previous_block.transactions]
+                for transaction_hash in transactions_hashes:
+                    if transaction_hash in previous_transactions_hashes:
+                        return False
+
+        return True
+
+    def __len__(self):
+        """
+        Return the length of the chain
+        :return:
+        """
+        return len(self.chain)
+
+    def merge(self, other):
+        """
+        Modify the blockchain if other is longer and valid.
+        :param other:
+        :return: True if the other chain is longer and valid, False otherwise
+        """
+        if other.validity() and len(self) < len(other):
+            self.chain = other.chain[:]
+            
+            for block in other.chain:
+                for transaction in block.transactions:
+                    if transaction not in self.mempool:
+                        self.mempool.append(transaction)
+
+            return True
+
+        else:
+            return False
+
+    def log(self):
+        print(self)
+        Transaction.log(self.mempool)
+
+        for b in self.chain:
+            b.log()
+
+
+def merge_test():
+    print('-----------merge_test-----------')
+    from ecdsa import SigningKey
+    blockchain = Blockchain()
+    sk = SigningKey.generate()
+    for i in range(100):
+        t = Transaction(f"Message {i}")
+        t.sign(sk)
+        blockchain.add_transaction(t)
+
+    blockchain2 = Blockchain()
+    sk2 = SigningKey.generate()
+    for i in range(100):
+        t = Transaction(f"Message {i}")
+        t.sign(sk2)
+        blockchain2.add_transaction(t)
+
+    for i in range(3):
+        b = blockchain.new_block()
+        b.mine()
+        blockchain.extend_chain(b)
+
+    for i in range(2):
+        b = blockchain2.new_block()
+        b.mine()
+        blockchain2.extend_chain(b)
+
+    blockchain.merge(blockchain2)
+    blockchain2.merge(blockchain)
+
+    for i in range(2):
+        b = blockchain.new_block()
+        b.mine()
+        blockchain.extend_chain(b)
+
+    for i in range(4):
+        b = blockchain2.new_block()
+        b.mine()
+        blockchain2.extend_chain(b)
+
+    blockchain.merge(blockchain2)
+    blockchain2.merge(blockchain)
+
+    blockchain.log()
+
+
+def simple_test():
+    print('-----------simple_test-----------')
+    from ecdsa import SigningKey
+    blockchain = Blockchain()
+    sk = SigningKey.generate()
+    for i in range(100):
+        t = Transaction(f"Message {i}")
+        t.sign(sk)
+        blockchain.add_transaction(t)
+
+    print(blockchain)
+    for i in range(3):
+        b = blockchain.new_block()
+        b.mine()
+        blockchain.extend_chain(b)
+
+    print(blockchain)
+    print(b.validity())
+    print(len(blockchain))
+
+def mon_test():
+    print('-----------mon_test-----------')
+    from ecdsa import SigningKey
+    blockchain = Blockchain()
+    sk = SigningKey.generate()
+
+if __name__ == '__main__':
+    print("Blockchain test")
+    simple_test()
+    merge_test()
---
+++ ecologic-credit-system/transaction.py
@@ -0,0 +1,214 @@
+"""
+This module define a transaction class. A transaction is a message signed by a private key.
+The signature can be verified with the public key.
+
+A transaction is a dictionary with the following keys:
+    - message: the message to sign
+    - date: the date of the transaction
+    - author: the hash of the public key
+    - vk: the public key
+    - signature: the signature of the message
+
+The signature and the public key are binary strings. Both are converted to hexadecimal (base64) string to be
+stored in the transaction.
+
+The hash of the transaction is the hash of the dictionary (keys are sorted).
+"""
+
+import utils
+import json
+import hashlib
+from ecdsa import VerifyingKey, BadSignatureError
+from rich.console import Console
+from rich.table import Table
+
+
+class IncompleteTransaction(Exception):
+    pass
+
+
+class Transaction(object):
+    def __init__(self, message, date=None, signature=None, vk=None, author=None):
+        """
+        Initialize a transaction. If date is None, the current time is used.
+        Signature and verifying key may be None.
+
+        Author is the hash of the verifying key (or None if vk is not specified).
+
+        :param message: str
+        :param date: str in format "%Y-%m-%d %H:%M:%S.%f" see (module "utils")
+        :param signature: str
+        :param vk: str
+        """
+        self.message = message
+
+        if date:
+            self.date = date
+        else:
+            self.date = utils.get_time()
+        
+        self.signature = signature
+        self.vk = vk
+
+        if vk:
+            self.author = hashlib.sha256(vk).hexdigest()
+        else:
+            self.author = author
+
+    def json_dumps(self):
+        """
+        Return a json representation of the transaction. The keys are sorted.
+        :return:
+        """
+        return json.dumps(self.data, sort_keys=True)
+
+    @property
+    def data(self):
+        d = {
+            "message": self.message,
+            "date": self.date,
+            "author": self.author,
+            "vk": self.vk
+        }
+        return d
+
+    def sign(self, sk):
+        """
+        Sign a transaction with a signing key. Set both attributes "signature" and "vk"
+        :param sk: A signing key (private)
+        """
+        self.vk = sk.verifying_key.to_pem().hex()
+
+        self.author = hashlib.sha256(self.vk.encode()).hexdigest()
+
+        self.signature = sk.sign(self.json_dumps().encode()).hex()
+
+    def verify(self):
+        """
+        Verify the signature of the transaction and the author.
+        :return: True or False
+        """
+        VK = VerifyingKey.from_pem(bytes.fromhex(self.vk))
+
+        binary_signature = bytes.fromhex(self.signature)
+
+        try:
+            VK.verify(binary_signature, self.json_dumps().encode())
+            return True  
+        except BadSignatureError:
+            return False  
+        
+
+
+
+    
+    def __str__(self):
+        """
+        :return: A string representation of the transaction
+        """
+        return f"Message:            {self.message}\nDate:               {self.date}\nVerification Key:   {self.vk}\nAuthor:             {self.author}\nSignature:          {self.signature}\n"
+
+    def __lt__(self, other):
+        """
+        Compare two transactions. The comparison is based on the hash of the transaction if it is defined else, the date.
+        :param other: a transaction 
+        :return: True or False
+        """
+        try: 
+            item1 = hashlib.sha256(self).digest()
+            item2 = hashlib.sha256(other).digest()
+
+            return item1 < item2
+        
+        except:
+            item1 = utils.str_to_time(self.date)
+            item2 = utils.str_to_time(other.date)
+
+            return item1 < item2
+
+    def hash(self):
+        """
+        Compute the hash of the transaction.
+
+        :raise IncompleteTransaction: if the transaction is not complete (signature or vk is None)
+        :return:
+        """
+        s = self.json_dumps().encode()
+        return hashlib.sha256(s).hexdigest()
+
+    @staticmethod
+    def log(transactions):
+        """
+        Print a nice log of set of the transactions
+        :param transactions:
+        :return:
+        """
+        table = Table(title=f"List of transactions")
+        table.add_column("Hash", justify="left", style="cyan")
+        table.add_column("Message", justify="left", style="cyan")
+        table.add_column("Date", justify="left", style="cyan")
+        table.add_column("Signature", justify="left", style="cyan")
+        table.add_column("Author", justify="left", style="cyan")
+
+        for t in sorted(transactions):
+            table.add_row(
+                None if t.vk is None else t.hash()[:14] + "...",
+                t.message,
+                t.date[:-7],
+                None if t.signature is None else t.signature[:7] + "...",
+                None if t.author is None else t.author[:7] + "..."
+            )
+
+        console = Console()
+        console.print(table)
+
+
+def test0():
+    print('-------------------test0-------------------')
+    from ecdsa import SigningKey, NIST384p
+    sk = SigningKey.generate(curve=NIST384p)
+    t1 = Transaction("One")
+    t2 = Transaction("Two")
+    t3 = Transaction("Three")
+    t4 = Transaction("Four")
+    t3.sign(sk)
+    t4.sign(sk)
+    Transaction.log([t1, t2, t3, t4])
+
+
+def test1():
+    print('-------------------test1-------------------')
+    from ecdsa import SigningKey, NIST384p
+    sk = SigningKey.generate(curve=NIST384p)
+    t = Transaction("Message de test")
+    print(t)
+    t.sign(sk)
+    print(t)
+    print(t.hash())
+    print(t.vk)
+    print(t.verify())
+    t.message += "2"
+    print(t.verify())
+
+
+def test2():
+    """
+    Warning: sk.verifying_key.to_pem().hex() produce a long string starting and ending with common information.
+    It is easy to manually check that the two strings are different, but it is not easy to see the difference.
+    This is the reason why the hash of the public key is used for author instead of the public key itself.
+    """
+    print('-------------------test2-------------------')
+    from ecdsa import SigningKey
+    sk = SigningKey.generate()
+    sk2 = SigningKey.generate()
+    print(sk.verifying_key.to_pem().hex())
+    print(sk2.verifying_key.to_pem().hex())
+    print(sk.verifying_key.to_pem().hex() == sk2.verifying_key.to_pem().hex())
+
+
+if __name__ == "__main__":
+    print("Test Transaction")
+    test0()
+    test1()
+    test2()
+
