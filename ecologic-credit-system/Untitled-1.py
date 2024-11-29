cfdcd254f6de3ee6b7cf14b24102923ae6d990e8
Fioletti Hugo <hugo.fioletti@student-cs.fr>
2024-11-29 10:17:04 +0100

    ignore

--- ecologic-credit-system/Untitled-1.py
+++ ecologic-credit-system/Untitled-1.py
@@ -1,31 +1,15 @@
-import re
+from transaction import Transaction
 
+Message = "test"
+Value = "-10"
+Destination = "daa1a9d79ed50144844d07276d282c1cf3227545f042f5e83f5d721ac521124c"
+Date = "2024-11-29 10:09:04.885948"
+Verification_Key ="2d2d2d2d2d424547494e205055424c4943204b45592d2d2d2d2d0a4d456b77457759484b6f5a497a6a3043415159494b6f5a497a6a3044415145444d674145706a34305a4537516a463636505a587638664762676e344749464b7064784b6f6d717437375673480a4f48476e742f47544f72774f72787238716e4674325755690a2d2d2d2d2d454e44205055424c4943204b45592d2d2d2d2d0a"
+Author = "daa1a9d79ed50144844d07276d282c1cf3227545f042f5e83f5d721ac521124c"
+Signature = "5506c6e1f9e73d63aca0b4ef24f13d9b2be88d030cacb77e58b493de99818d6a8d386e307c8a1d6d336be7f7e3ad8b12"
 
-pattern = r"^[^:]+(?:\s+[^:]+)*\s*:\s*[-+][0-9]+$"
+t = Transaction(Message, Value, Destination, Date, Signature ,Verification_Key, Author)
 
-def test():
-    string = None
-    while string!='end':
-        string = input()
-        print(re.match(pattern, string))
+print(t.verify())
 
-#test()
-
-print(int('+43'))
-print(int('-31'))
-
-print(len('8c7a2776446cec5519df357acbb06e5af2acefa6ccbf9f2015f3482aa6c00cb2'))
-
-import ecdsa
-
-# Générer une clé privée
-sk = ecdsa.SigningKey.generate(curve=ecdsa.SECP256k1)
-
-# Obtenir la clé publique à partir de la clé privée
-vk = sk.get_verifying_key()
-
-# Convertir la clé publique en format hexadécimal
-public_key_hex = vk.to_string().hex()
-
-# Afficher la clé publique
-print(f"Clé publique (hexadécimale) : {public_key_hex}")
+print(len(Destination))
